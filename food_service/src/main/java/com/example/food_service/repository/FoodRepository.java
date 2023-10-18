2c827a8b95bc6d06d95033745cd73dbfc54a798a
apple <zamersbouy@gmail.com>
2023-10-18 23:13:58 +0530

    removed commit

--- food_service/src/main/java/com/example/food_service/repository/FoodRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.food_service.repository;
-
-import com.example.food_service.entity.Food;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FoodRepository extends JpaRepository<Food,String> {
-}
