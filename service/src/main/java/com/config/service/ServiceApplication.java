e6d851c365c29d712328a9ed54569f839a0252f1
apple <zamersbouy@gmail.com>
2023-11-03 21:53:43 +0530

    config service

---
+++ service/src/main/java/com/config/service/ServiceApplication.java
@@ -0,0 +1,17 @@
+package com.config.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceApplication.class, args);
+	}
+
+}
